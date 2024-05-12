bb3fb689d982f36eb4a9ac9f3f0c631c29ff4e16
jaylowanshi <jaylowanshi3@gmail.com>
2024-05-12 23:36:28 +0530

    String  operations

---
+++ stringOperation.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class stringOperation {
+    public static void main(String[] args)
+    {
+        String s = "Sachin";
+
+        // concat() method appends
+        // the string at the end
+        s.concat(" Tendulkar");
+
+        // This will print Sachin
+        // because strings are
+        // immutable objects
+        System.out.println(s);
+    }
+}
