c18771d25d03961ed59ec07af39cfbfdb9903877
jaylowanshi <jaylowanshi3@gmail.com>
2024-05-15 11:35:24 +0530

    leapyear program

---
+++ ArrayList/Example1.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+import java.util.*;
+public class Example1{
+    public static void main(String args[]){
+        ArrayList<String> list=new ArrayList<String>();//Creating arraylist
+        list.add("Mango");//Adding object in arraylist
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+
+        System.out.println(list);
+    }
+}
